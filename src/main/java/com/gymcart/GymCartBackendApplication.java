0c9993f2380634adcdd1868529b7534b26b9fa8c
IRFAD K P <IRFAD.K.P@ibm.com>
2025-04-20 15:59:29 +0530

    Initial Spring Boot setup with HelloController, Product entity, and H2 database

---
+++ src/main/java/com/gymcart/GymCartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gymcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymCartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymCartBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gymcart/GymCart_Backend/GymCartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymcart.GymCart_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymCartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
